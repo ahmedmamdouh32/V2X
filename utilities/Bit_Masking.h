45631b6377fac7a3f1ef7f12367a3ac67ead4363
ahmedmamdouh32 <ahmedmamdouh.am32@gmail.com>
2023-12-08 20:41:48 +0200

    Latest Verison

--- utilities/Bit_Masking.h
+++
@@ -1,9 +0,0 @@
-#ifndef __BIT_MASKING_H__
-#define __BIT_MASKING_H__
-
-#define SET_BIT(REG,BIT)		(REG |= (1 << BIT))
-#define CLEAR_BIT(REG,BIT)		(REG &= ~(1 << BIT))
-#define TOG_BIT(REG,BIT)		(REG ^= (1 << BIT))
-#define GET_BIT(REG,BIT)		((REG >> BIT) & 0x01)
-
-#endif
